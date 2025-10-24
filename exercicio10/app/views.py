33e87d143a24235cb3ebf547c13b4ba3f57515bc
fbguimaraes <fbguimaraes.ads@gmail.com>
2025-10-24 09:08:51 -0300

    Enviando exercicios restantes

---
+++ exercicio10/app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def lista_posts(request):
+    return HttpResponse(" Lista de posts do blog.")
+
+
+def post(request, ano, mes, slug):
+    ano = int(ano)
+    mes = int(mes)
+
+    if not (2000 <= ano <= 2030):
+        return HttpResponse(" Ano inválido! O ano deve estar entre 2000 e 2030.")
+
+    if not (1 <= mes <= 12):
+        return HttpResponse(" Mês inválido! O mês deve estar entre 01 e 12.")
+
+    # Se tudo estiver ok, exibe o post
+    return HttpResponse(f" Exibindo o post '{slug.replace("-", " ").title()}' publicado em {mes:02d}/{ano}.")
